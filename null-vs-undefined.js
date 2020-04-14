28b49cf9705407c2b95b31553a10d8dd7002190d
SH-EMONe <sh.emon1020@gmail.comm>
2020-04-14 14:25:41 -0700

    null vs undefined is cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+let car;
+console.log(car);
+
+
+function add(num1,num2){
+    
+}
+let result = add(5,10);
+console.log(result);
+
+
+function minus(num3,num4){
+    return num3 ,num4;
+} 
+let result2 = minus(15);
+console.log(result2);
+
+
+let student ={name:"Abul", class:"six", roll:"10"}
+console.log(student.school);
+
