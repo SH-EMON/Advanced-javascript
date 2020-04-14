2e83143e67bf4354c1574d31178d17d5bdc08f68
SH-EMONe <sh.emon1020@gmail.comm>
2020-04-14 13:50:24 -0700

    true false condition related interview question is cleared

---
+++ true-false.js
@@ -0,0 +1,115 @@
+//Example of truthy condition
+
+const num1 = 10;
+if(num1 > 5){
+    console.log("Condition number one is true");
+}else{
+    console.log("Condition number one is false");
+}
+
+const num2 = 10;
+if(num2){
+    console.log("Condition number two is true");
+}else{
+    console.log("Conditionn number two is false");
+}
+
+const num3 = -10;
+if(num3){
+    console.log("Condition number three is true");
+}else{
+    console.log("Condition number three is false");
+}
+
+const name1 ="Abbas";
+if(name1.length > 4){
+    console.log("Condition number four is true");
+}else{
+    console.log("Condition number four is false");
+}
+
+const name2 ="Sabbas";
+if(name2){
+    console.log("Condition number five is true");
+}else{
+    console.log("Condition number five is false");
+}
+
+const num4 = "0";
+if(num4){
+    console.log("Condition number six is true");
+}else{
+    console.log("Condition number six is false");
+}
+
+const num5 = " ";
+if(num5){
+    console.log("Condition number seven is true");
+}else{
+    console.log("Condition number seven is false");
+}
+
+const num6 = [];
+if(num6){
+    console.log("Condition number eight is true");
+}else{
+    console.log("Condition number eight is false");
+}
+
+const num7 = {};
+if(num7){
+    console.log("Condition number nine is true");
+}else{
+    console.log("Condition number nine is false");
+}
+
+const num8 = "...";
+if(num8){
+    console.log(".................................true");
+}else{
+    console.log("_________________________________false");
+};
+
+// Example of falsy condition
+
+const num9 = 0;
+if(num9){
+    console.log("Condition number eleven is true");
+}else{
+    console.log("Condition number eleven is false");
+}
+
+const num10 = "10";
+if(num10 > 20){
+    console.log("Condition number twelve is true");
+}else{
+    console.log("Condition number twelve is false");
+}
+
+const name3 ="";
+if(name3){
+    console.log("Condition number thirty is true");
+}else{
+    console.log("Condition number thirty is false");
+}
+
+const name4 = null;
+if(name4){
+    console.log("Condition number fourteen is true");
+}else{
+    console.log("Condition number fourteen is false");
+}
+
+const name5 =false;
+if(name5){
+    console.log("Condition number fifteen is true");
+}else{
+    console.log("Condition number fifteen is false");
+}
+
+const name6 = undefined;
+if(name6){
+    console.log("Condition number sixteen is true");
+}else{
+    console.log("Condition number sixteen is false");
+}
